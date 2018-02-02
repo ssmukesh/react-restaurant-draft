c7e359037a91fa5ca176a3da6fd3027a73efadb5
Mukesh Selvaraj <33991311+ssmukesh@users.noreply.github.com>
2018-02-02 09:27:31 +0530

    created
    Signed-off-by: Mukesh Selvaraj <33991311+ssmukesh@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Search your restaurant online in UAE</Text>
+        <Text>Choose Your City</Text>
+        <Text>© 2018 - Copyright Lunad Media & Communication LLC.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
